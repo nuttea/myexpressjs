8bffc95654a58972086368b27009e7310d31cccd
nuttea <nuttea@gmail.com>
2016-03-03 13:56:28 +0800

    correct

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello from Cisco Shipped!' <p> Thanks for learning SHipped!</p>);
+    res.send('Hello from Cisco Shipped! <p> Thanks for learning SHipped!</p>');
 });
 
 var server = app.listen(3000, function () {
